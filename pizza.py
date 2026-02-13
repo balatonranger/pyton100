fda634f38dda8cded68f7edabca4f3ccbe0a07ec
balatonranger <balaton.ranger@gmail.com>
2026-02-13 13:00:06 +0100

    pizza ordering

---
+++ pizza.py
@@ -0,0 +1,21 @@
+print("Welcome to the pizza delivery service!")
+size = input("What size pizza do you want? S, M, or L: ")
+add_pepperoni = input("Do you want pepperoni? Y or N: ")
+add_cheese = input("Do you want extra cheese? Y or N: ")
+bill = 0
+if size == "S":
+    bill += 15
+elif size == "M":
+    bill += 20
+elif size == "L":
+    bill += 25
+else:
+    print("Invalid size selected.")
+if add_pepperoni == "Y":
+    if size == "S":
+        bill += 2
+    else:
+        bill += 3
+if add_cheese == "Y":
+    bill += 1
+print(f"Your final bill is: ${bill}.")
