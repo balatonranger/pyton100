3451ec2bc12c4c863540d80cdaf1d3f66aa04f4d
balatonranger <balaton.ranger@gmail.com>
2026-03-05 11:32:14 +0100

    day 6

---
+++ day6.py
@@ -0,0 +1,14 @@
+#built-in functions
+print("Hello, World!")
+num_chars = len("Hello, World!")
+print(num_chars)
+#our own functions
+def my_function():
+    print("Hello")
+    print("Bye")
+my_function()
+#1st define the function
+# 2nd call the function
+
+#Indentation
+#like the file structure of a computer, the indentation of your code is important. It tells Python which lines of code belong to which functions. In the example above, the two print statements are indented under the function definition, which means they are part of the function. If you were to remove the indentation, you would get an error because Python would not know which lines of code belong to the function.
